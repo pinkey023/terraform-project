6ac5ef97ceb70e82e0be33514588f4804021710f
pinkey023 <132822194+pinkey023@users.noreply.github.com>
2023-08-18 19:03:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-region = "us-east-1"
+region = "ap-south-1"
 access_key = "AKIAUYBTQQLGXE3XTSM3"
 secret_key = "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"
 }
 resource "aws_instance" "one" {
-ami = "ami-0453898e98046c639"
-key_name = "hreyan"
+ami = "ami-0d951b011aa0b2c19"
+key_name = "honey"
 instance_type = "t2.micro"
-availability_zone = "us-east-1a"
+availability_zone = "ap-south-1a"
 vpc_security_group_ids = [aws_security_group.five.id]
 user_data = <<EOF
 #!/bin/bash
@@ -22,10 +22,10 @@ Name = "webserver-1"
 }
 }
 resource "aws_instance" "two" {
-ami = "ami-0453898e98046c639"
-key_name = "hreyan"
+ami = "ami-0d951b011aa0b2c19"
+key_name = "honey"
 instance_type = "t2.micro"
-availability_zone = "us-east-1b"
+availability_zone = "ap-south-1b"
 vpc_security_group_ids = [aws_security_group.five.id]
 user_data = <<EOF
 #!/bin/bash
@@ -40,20 +40,20 @@ Name = "webserver-2"
 }
 }
 resource "aws_instance" "three" {
-ami = "ami-0453898e98046c639"
-key_name = "hreyan"
+ami = "ami-0d951b011aa0b2c19"
+key_name = "honey"
 instance_type = "t2.micro"
-availability_zone = "us-east-1a"
+availability_zone = "ap-south-1a"
 vpc_security_group_ids = [aws_security_group.five.id]
 tags = {
 Name = "appserver-1"
 }
 }
 resource "aws_instance" "four" {
-ami = "ami-0453898e98046c639"
-key_name = "hreyan"
+ami = "ami-0d951b011aa0b2c19"
+key_name = "honey"
 instance_type = "t2.micro"
-availability_zone = "us-east-1b"
+availability_zone = "ap-south-1b"
 vpc_security_group_ids = [aws_security_group.five.id]
 tags = {
 Name = "appserver-2"
@@ -78,24 +78,24 @@ resource "aws_s3_bucket" "six" {
 bucket = "mintu00113344"
 }
 resource "aws_iam_user" "seven" {
-for_each = var.user_name
+for_each = var.user_names
 name = each.value
 }
-variable "user_name" {
+variable "user_names" {
 description = "*"
 type = set(string)
 default = ["user-1", "user-2", "user-3", "user-4"]
 }
 resource "aws_ebs_volume" "eight" {
 size = 40
-availability_zone = "us-east-1a"
+availability_zone = "ap-south-1a"
 tags = {
 Name = "vol-1"
 }
 }
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
-  availability_zones = ["us-east-1a", "us-east-1b"]
+
+  availability_zones = ["ap-south-1a", "ap-south-1b"]
 
   listener {
     instance_port     = 80
