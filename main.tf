ba36bd6cae19ad69323154cf7dc35468805530fa
pinkey023 <132822194+pinkey023@users.noreply.github.com>
2023-08-18 14:01:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,113 +1,115 @@
-#this file consists of code for instances and sg
 provider "aws" {
-region = "us-east-1"
-access_key = ""
-secret_key = ""
 }
-
 resource "aws_instance" "one" {
-  ami             = "ami-04823729c75214919"
-  instance_type   = "t2.micro"
-  key_name        = "mintu"
-  vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "us-east-1a"
-  user_data       = <<EOF
+ami = "ami-0453898e98046c639"
+instance_type = "t2.micro"
+availability_zone = "us-east-1a"
+vpc_security_group_ids = [aws_security_group.five.id]
+user_data = <<EOF
 #!/bin/bash
 sudo -i
 yum install httpd -y
 systemctl start httpd
 chkconfig httpd on
-echo "hai all this is my app created by terraform infrastructurte by raham sir server-1" > /var/www/html/index.html
+echo "hai this is my app created by terraform in server-1" >/var/www/html
 EOF
-  tags = {
-    Name = "web-serverr-1"
-  }
+tags = {
+Name = "webserver-1"
 }
-
+}
 resource "aws_instance" "two" {
-  ami             = "ami-04823729c75214919"
-  instance_type   = "t2.micro"
-  key_name        = "mintu"
-  vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "us-east-1b"
-  user_data       = <<EOF
+ami = "ami-0453898e98046c639"
+instance_type = "t2.micro"
+availability_zone = "us-east-1b"
+vpc_security_group_ids = [aws_security_group.five.id]
+user_data = <<EOF
 #!/bin/bash
 sudo -i
 yum install httpd -y
 systemctl start httpd
 chkconfig httpd on
-echo "hai all this is my website created by terraform infrastructurte by raham sir server-2" > /var/www/html/index.html
+echo "hai this is my app created by terraform in server-2" >/var/www/html
 EOF
-  tags = {
-    Name = "web-server-2"
-  }
+tags = {
+Name = "webserver-2"
 }
-
+}
 resource "aws_instance" "three" {
-  ami             = "ami-04823729c75214919"
-  instance_type   = "t2.micro"
-  key_name        = "mintu"
-  vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "app-server-1"
-  }
+ami = "ami-0453898e98046c639"
+instance_type = "t2.micro"
+availability_zone = "us-east-1a"
+vpc_security_group_ids = [aws_security_group.five.id]
+tags = {
+Name = "appserver-1"
 }
-
+}
 resource "aws_instance" "four" {
-  ami             = "ami-04823729c75214919"
-  instance_type   = "t2.micro"
-  key_name        = "mintu"
-  vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "us-east-1b"
-  tags = {
-    Name = "app-server-2"
-  }
+ami = "ami-0453898e98046c639"
+instance_type = "t2.micro"
+availability_zone = "us-east-1b"
+vpc_security_group_ids = [aws_security_group.five.id]
+tags = {
+Name = "appserver-2"
 }
-
+}
 resource "aws_security_group" "five" {
-  name = "elb-sg"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+name = "pinkey-sg"
+ingress {
+from_port = 80
+to_port = 80
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
 }
-
+}
+egress {
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+}
+}
 resource "aws_s3_bucket" "six" {
-  bucket = "niroopainkey006688"
+bucket = "mintu00113344"
 }
-
 resource "aws_iam_user" "seven" {
-for_each = var.user_names
+for_each = var.user_name
 name = each.value
 }
-
-variable "user_names" {
+variable "user_name" {
 description = "*"
 type = set(string)
-default = ["user1", "user2", "user3", "user4"]
+default = ["user-1,user-2,user-3,user-4"]
 }
-
 resource "aws_ebs_volume" "eight" {
- availability_zone = "us-east-1a"
-  size = 40
+size = 40
+availability_zone = "us-east-1a"
+tags = {
+Name = "vol-1"
+}
+}
+resource "aws_elb" "bar" {
+  name               = "raham-terraform-elb"
+  availability_zones = ["us-east-1a", "us-east-1b"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 5
+    timeout             = 5
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  instances                 = ["${aws_instance.one.id}", "${aws_instance.two.id}"]
+  cross_zone_load_balancing = true
+  idle_timeout              = 400
   tags = {
-    Name = "ebs-0001"
+    Name = "raham-tf-elb"
   }
 }
